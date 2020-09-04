08bf754dd078136f6ce346ed6387d326f6d69ca9
Ben Thomson <git@alfreido.com>
2020-09-04 12:24:45 +0800

    Require PHP 7.2 or above

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 #!/usr/bin/env php
 <?php
 
+if (version_compare(PHP_VERSION, '7.2.0', '<')) {
+    echo 'This program requires PHP 7.2 or above to be installed.';
+    exit(1);
+}
+
 // Load dependencies
 require __DIR__.'/vendor/autoload.php';
 
