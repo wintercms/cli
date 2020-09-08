c6ed3860d66c8d2f59c53f0253b3bb234a8b0503
Ben Thomson <git@alfreido.com>
2020-09-08 15:57:36 +0800

    Add GitHub token registration command

---
+++ src/Commands/GithubToken/Command.php
@@ -0,0 +1,57 @@
+<?php namespace BennoThommo\OctoberCli\Commands\GithubToken;
+
+use BennoThommo\OctoberCli\BaseCommand;
+use BennoThommo\OctoberCli\GitHub\Token;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * GitHub token command
+ *
+ * @since 0.2.1
+ * @author Ben Thomson
+ */
+class Command extends BaseCommand
+{
+    /**
+     * @inheritDoc
+     */
+    protected static $defaultName = 'github:token';
+
+    /**
+     * @inheritDoc
+     */
+    protected function configure()
+    {
+        $this
+            // the short description shown while running "php bin/console list"
+            ->setDescription('Sets the GitHub Access token for the helper.')
+
+            // the full command description shown when running the command with
+            // the "--help" option
+            ->setHelp(
+                'Allows you to store a GitHub Access Token locally. This is used for some functions of the CLI helper'
+                . ' which may be subject to GitHub API\'s rate limits.'
+            )
+
+            // arguments
+            ->addArgument(
+                'token',
+                InputArgument::REQUIRED,
+                'The GitHub Access Token to store'
+            )
+        ;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $token = new Token();
+        $path = $token->write($input->getArgument('token'));
+
+        $this->success('Token written to ' . $path);
+    }
+}
---
+++ src/GitHub/Token.php
@@ -0,0 +1,102 @@
+<?php namespace BennoThommo\OctoberCli\GitHub;
+
+use Exception;
+
+/**
+ * Simple token holder.
+ *
+ * Stores a GitHub access token in your home directory. The token will retrieve the following permissions:
+ *  - repo:public_repo (Access public repositories)
+ *
+ * @since 0.2.1
+ * @author Ben Thomson
+ */
+class Token
+{
+    /** @var string stored token */
+    protected $token = null;
+
+    /** @var array suitable locations to find and store the token */
+    protected $suitableLocations = [];
+
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+        $this->suitableLocations = [
+            $_SERVER['HOME'] . DIRECTORY_SEPARATOR . '.config',
+            $_SERVER['HOME'] . DIRECTORY_SEPARATOR . '.local' . DIRECTORY_SEPARATOR . 'share',
+            $_SERVER['HOME'] . DIRECTORY_SEPARATOR . 'AppData' . DIRECTORY_SEPARATOR . 'Local',
+            $_SERVER['HOME'] . DIRECTORY_SEPARATOR . '.october-cli',
+        ];
+    }
+
+    /**
+     * Writes the token to an available suitable location.
+     *
+     * @param string $token
+     * @return string Path to the token file.
+     * @throws Exception If a suitable location cannot be found.
+     */
+    public function write(string $token): string
+    {
+        $written = null;
+
+        foreach ($this->suitableLocations as $dir) {
+            if (is_dir($dir)) {
+                if (!is_writable($dir)) {
+                    continue;
+                }
+                if (!is_dir($dir . DIRECTORY_SEPARATOR . 'october-cli')) {
+                    mkdir($dir . DIRECTORY_SEPARATOR . 'october-cli');
+                }
+
+                $path = $dir . DIRECTORY_SEPARATOR . 'october-cli' . DIRECTORY_SEPARATOR . 'token';
+
+                file_put_contents($path, $token);
+                $written = $path;
+                break;
+            }
+        }
+
+        if (!$written) {
+            throw new Exception('Unable to write the GitHub Access Token in your home directory.');
+        }
+
+        return $written;
+    }
+
+    /**
+     * Get the stored token.
+     *
+     * @return string
+     * @throws Exception If no stored token is found.
+     */
+    public function read(): string
+    {
+        if (!is_null($this->token)) {
+            return $this->token;
+        }
+
+        foreach ($this->suitableLocations as $dir) {
+            $path = $dir . DIRECTORY_SEPARATOR . 'october-cli' . DIRECTORY_SEPARATOR . 'token';
+
+            if (file_exists($path)) {
+                $this->token = file_get_contents($path);
+                break;
+            }
+        }
+
+        if ($this->token === null) {
+            throw new Exception(
+                'You must provide a GitHub Access Token for October CLI Helper to use this feature.'
+                . ' Please visit' . "\n"
+                . 'https://github.com/settings/tokens/new?scopes=public_repo&description=October%20CLI%20Helper'
+                . ' to create a token, then use the "github:token" command to store the token for further use.'
+            );
+        }
+
+        return $this->token;
+    }
+}
