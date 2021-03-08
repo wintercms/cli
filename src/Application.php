4b37b3256a4a30445ed1534b2459712bea848c14
Ben Thomson <git@alfreido.com>
2021-03-08 16:36:39 +0800

    Add a fully-sick logo

--- src/Application.php
+++ src/Application.php
@@ -11,9 +11,20 @@ use Symfony\Component\Console\Application as SymfonyApplication;
  */
 class Application extends SymfonyApplication
 {
-    protected static $name = "\033[1;34mWinter\033[0m CLI";
+    protected static $name = "Winter \033[1;34mCLI\033[0m";
 
     protected static $version = '@version@ (@datetime@)';
+
+    protected static $logo = <<<LOGO
+
+db   d8b   db d888888b d8b   db d888888b d88888b d8888b.       \033[1;34m...\033[0m
+88   I8I   88   `88'   888o  88 `~~88~~' 88'     88  `8D  \033[1;34m... ..... ...\033[0m
+88   I8I   88    88    88V8o 88    88    88ooooo 88oobY'    \033[1;34m.. ... ..\033[0m
+Y8   I8I   88    88    88 V8o88    88    88~~~~~ 88`8b      \033[1;34m.. ... ..\033[0m
+`8b d8'8b d8'   .88.   88  V888    88    88.     88 `88.  \033[1;34m... ..... ...\033[0m
+ `8b8' `8d8'  Y888888P VP   V8P    YP    Y88888P 88   YD       \033[1;34m...\033[0m
+\n
+LOGO;
 
     /**
      * Winter CLI constructor.
@@ -29,6 +40,16 @@ class Application extends SymfonyApplication
         $this->setVersion(static::$version);
 
         $this->detectCommands();
+    }
+
+    /**
+     * Gets the help message.
+     *
+     * @return string A help message
+     */
+    public function getHelp()
+    {
+        return static::$logo . parent::getHelp();
     }
 
     /**
